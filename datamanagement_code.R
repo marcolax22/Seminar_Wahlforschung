d7902427a6f81f5bc8827edb6bd14606d49e1ccd
marcolax22 <86615981+marcolax22@users.noreply.github.com>
2021-09-24 15:09:12 +0100

    Update datamanagement_code.R

--- datamanagement_code.R
+++ datamanagement_code.R
@@ -27,11 +27,12 @@ ZA5322_birth <- read_dta(file.path("data","20210830_Shikano_ZA5322_v1-1-0.dta"))
 
 
 
+
 ###################################################################################
 
 #preparing the data set: ZA5770.dta------------------------------------------------
 
-ZA5770 <- ZA5770 %>% select(lfdn, j, k, l, m, n, jahr, jhhchild18, j60_2, n60_2,
+ZA5770 <- ZA5770 %>% select(lfdn, jahr, j, k, l, m, n, jhhchild18, j60_2, n60_2,
                             j65a, j65b, j64a, j64b, m65a, m65b, m64a, m64b, j69a, n69a,
                             j69b, n69b, j70a, j70b, n70a, n70b, 
                             jpidstrk, npidstrk, jpid_a, jpid_b, lfdn_lfp09)
@@ -50,12 +51,32 @@ ZA5770 <- ZA5770 %>% rename(PID_2013 = jpidstrk,
 
 #preparing the data set: ZA5320.dta------------------------------------------------
 
+ZA5320 <- ZA5320 %>% select(id, cjahr, cpid, cpidstrk, dpid, dpidstrk, epid, epidstrk)
+
+ZA5320 <- ZA5320 %>% rename(lfdn = id,
+                            pid_2002 = cpid,
+                            PID_2002 = cpidstrk,
+                            pid_2005 = dpid,
+                            PID_2005 = dpidstrk,
+                            pid_2009 = epid,
+                            PID_2009 = epidstrk)
 
 #preparing the data set: ZA5321.dta------------------------------------------------
 
+ZA5321 <- ZA5321 %>% select(lfdn, jahr, dpidstrk, epidstrk, fpidstrk, hpidstrk, jpidstrk)
+
+ZA5321 <- ZA5321 %>% rename(PID_2005 = dpidstrk,
+                            PID_2007 = epidstrk,
+                            PID_2009 = fpidstrk,
+                            PID_2011 = hpidstrk,
+                            PID_2013 = jpidstrk)
 
 #preparing the data set: ZA5322.dta------------------------------------------------
 
+ZA5322 <- ZA5322 %>% select(lfdn, jahr, hpidstrk, jpidstrk)
+
+ZA5322 <- ZA5322 %>% rename(PID_2011 = hpidstrk,
+                            PID_2013 = jpidstrk)
 
 #preparing the data set: ZA5770_birth----------------------------------------------
 
@@ -80,11 +101,14 @@ ZA5770_birth <- ZA5770_birth %>% select(lfdn,monat)
 ZA5320_birth <- ZA5320_birth %>% 
   select(id, cmonat, dmonat, emonat)
 
-ZA5320_birth$monat <- ifelse(ZA5320_birth$cmonat,
-                             ifelse(ZA5320_birth$dmonat,
-                                    ifelse(ZA5320_birth$emonat, NA)))
+ZA5320_birth$monat <- ifelse(ZA5320_birth$cmonat <
+                               13, ZA5320_birth$cmonat,
+                             ifelse(ZA5320_birth$dmonat <
+                                      13, ZA5320_birth$dmonat,
+                                    ifelse(ZA5320_birth$emonat <
+                                             13, ZA5320_birth$emonat, NA)))
 
-ZA5770_birth <- ZA5770_birth %>% select(lfdn,monat)
+ZA5320_birth <- ZA5320_birth %>% select(id,monat)
 
 #preparing the data set: ZA5321_birth----------------------------------------------
 
@@ -121,13 +145,11 @@ ZA5322_birth <- ZA5322_birth %>% select(lfdn,monat)
 
 
 
-
-
 ###################################################################################
 
 #joining the data set ZA5770 and 5770_birth----------------------------------------
 
-ZA5770_full <- full_join(ZA5770.prepare, ZA5770_birth.prepare, by = "lfdn", na_matches = "never")
+ZA5770_full <- full_join(ZA5770, ZA5770_birth, by = "lfdn", na_matches = "never")
 
 #joining month and year------------------------------------------------------------
 
@@ -139,103 +161,68 @@ ZA5770_full <- ZA5770_full %>% unite(month_year, monat, jahr, sep = "-") %>%
 
 
 
-
 #joining data set ZA5320 and ZA5320_birth------------------------------------------
+
+ZA5320_birth <- ZA5320_birth %>% rename(lfdn = id)
+ZA5320_full <- full_join(ZA5320, ZA5320_birth, by = "lfdn", na_matches = "never")
 
 #joining month and year------------------------------------------------------------
 
+ZA5320_full$jahr <- as_factor(ZA5320_full$cjahr)
 
+ZA5320_full <- ZA5320_full %>% unite(month_year, monat, cjahr, sep = "-") %>% 
+  mutate(month_year = as.yearmon(month_year, "%m-%Y")) %>% 
+  mutate(month_year = as.Date(month_year))
 
 
 
 #joining data set ZA5321 and ZA5321_birth------------------------------------------
 
+ZA5321_full <- full_join(ZA5321, ZA5321_birth, by = "lfdn", na_matches = "never")
+
 #joining month and year------------------------------------------------------------
 
+ZA5321_full$jahr <- as_factor(ZA5321_full$jahr)
 
+ZA5321_full <- ZA5321_full %>% unite(month_year, monat, jahr, sep = "-") %>% 
+  mutate(month_year = as.yearmon(month_year, "%m-%Y")) %>% 
+  mutate(month_year = as.Date(month_year))
 
 
 
 #joining ZA5322 and ZA5322_birth---------------------------------------------------
 
+ZA5322_full <- full_join(ZA5322, ZA5322_birth, by = "lfdn", na_matches = "never")
+
 #joining month and year------------------------------------------------------------
 
+ZA5322_full$jahr <- as_factor(ZA5322_full$jahr)
+
+ZA5322_full <- ZA5322_full %>% unite(month_year, monat, jahr, sep = "-") %>% 
+  mutate(month_year = as.yearmon(month_year, "%m-%Y")) %>% 
+  mutate(month_year = as.Date(month_year))
+
+###################################################################################
 
 
 
 
 ###################################################################################
 
-
-
-
-###################################################################################
-
-#Saving the data set with merged data ZA5770---------------------------------------
+#Saving the data set with merged data ZA5770
 
 save(ZA5770_full, file = "data/ZA5770_full.RData")
 
 #Saving the data set with merged data ZA5320
 
+save(ZA5320_full, file = "data/ZA5320_full.RData")
+
 #Saving the data set with merged data ZA5321
+
+save(ZA5321_full, file = "data/ZA5321_full.RData")
 
 #Saving the data set with merged data ZA5322
 
-###################################################################################
-
-
-
-
-
-
+save(ZA5322_full, file = "data/ZA5322_full.RData")
 
 ###################################################################################
-
-#dataset for RDD-------------------------------------------------------------------
-
-load("data/ZA5770_full.RData")
-
-ZA_RDD <- ZA5770_full %>% filter(month_year >= "1992-01-01")
-ZA_RDD2 <- ZA5770_full %>% filter(month_year >= "1990-01-01" & month_year <= "1993-01-01")
-
-#preparing the dataset for RDD and RDD2--------------------------------------------
-
-class(ZA_RDD$jpid_a)
-ZA_RDD$jpid_a <- as.numeric(ZA_RDD$jpid_a)
-ZA_RDD$PID_2013 <- as.numeric(ZA_RDD$PID_2013)
-
-na_strings <- c("-97")
-
-ZA_RDD <- ZA_RDD %>% replace_with_na_all(condition = ~ .x %in% na_strings |(as.integer(.x) < 0))
-
-ZA_RDD$PID_2013[is.na(ZA_RDD$PID_2013)] = 0    
-
-#-----------------------
-
-ZA_RDD2$PID_2013 <- as.numeric(ZA_RDD2$PID_2013)
-
-na_strings <- c("-97")
-
-ZA_RDD2 <- ZA_RDD2 %>% replace_with_na_all(condition = ~ .x %in% na_strings |(as.integer(.x) < 0))
-
-ZA_RDD2$PID_2013[is.na(ZA_RDD2$PID_2013)] = 0  
-
-#---------------------------------------------------------------------------------
-
-###################################################################################
-
-
-
-
-
-###################################################################################
-
-#Saving the data set---------------------------------------------------------------
-
-save(ZA_RDD, file = "data/ZA_RDD.RData")
-save(ZA_RDD2, file= "data/ZA_RDD2.RData")
-
-###################################################################################
-
-
-
